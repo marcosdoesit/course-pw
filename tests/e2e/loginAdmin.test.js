6b152b775e8add5be9a8b5e8ced210e2ab1d2c75
Marcos Souza <marcosfromrio@protonmail.com>
2024-10-28 13:59:15 -0300

    test: add wrong pass admin login case

--- tests/e2e/loginAdmin.test.js
+++ tests/e2e/loginAdmin.test.js
@@ -7,8 +7,16 @@ test.beforeEach(async ({ page }) => {
   loginPage = new LoginAdminPage(page);
 });
 
+/* positive tests */
 test("Should login as admin", async ({ page }) => {
   await loginPage.open();
   await loginPage.submitLoginForm("admin@zombieplus.com", "pwd123");
   await loginPage.checkLoggedIn();
+});
+
+/* negative tests */
+test("Shouldn't login with wrong pass", async ({ page }) => {
+  await loginPage.open();
+  await loginPage.submitLoginForm("admin@zombieplus.com", "wrongPassword");
+  await loginPage.checkHasToastText(/Ocorreu um erro/);
 });
--- tests/pages/LoginAdminPage.js
+++ tests/pages/LoginAdminPage.js
@@ -21,6 +21,12 @@ class LoginPage {
     await this.page.waitForLoadState("domcontentloaded");
     await expect(this.page).toHaveURL(/\/admin\/movies/);
   }
+
+  async checkHasToastText(message) {
+    const toast = await this.page.locator(".toast");
+    await expect(toast).toHaveText(message);
+    await expect(toast).not.toBeVisible({ timeout: 5000 });
+  }
 }
 
 module.exports = LoginPage;
