1595950523dc74c2d550617abbfbfb31c75a8159
Marcos Souza <marcosfromrio@protonmail.com>
2024-10-31 11:27:31 -0300

    chore: add dotenv for environment variable management and update Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 const tsconfigPaths = require('tsconfig-paths');
+import { config } from 'dotenv';
+
+config();
 
 tsconfigPaths.register({
   baseUrl: '.',
@@ -30,14 +33,15 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    baseURL: process.env.BASE_URL,
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
