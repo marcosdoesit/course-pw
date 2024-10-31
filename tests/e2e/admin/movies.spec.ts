a815f3bad9dbccc1ac8255e1a9f27f4acf01ca46
Marcos Souza <marcosfromrio@protonmail.com>
2024-10-31 13:16:37 -0300

    chore: refactor movie tests to TypeScript and enhance MoviesPage class

--- tests/e2e/admin/movies.spec.ts
+++ tests/e2e/admin/movies.spec.ts
@@ -1,11 +1,15 @@
-const { test } = require('@playwright/test');
+import { test } from '@playwright/test';
 
 import { LoginAdminPage } from '@pages/admin';
 import { MoviesPage } from '@pages/admin/Movies';
 
 import { ToastComponent } from '@components/Toast';
 
-let loginPage, toast, moviesPage;
+import moviesData from '@support/fixtures/movies.json';
+
+let loginPage: LoginAdminPage,
+  toast: ToastComponent,
+  moviesPage: MoviesPage;
 
 test.beforeEach(async ({ page }) => {
   loginPage = new LoginAdminPage(page);
@@ -17,5 +21,16 @@ test('Admin can add a movie', async ({ page }) => {
   await loginPage.open();
   await loginPage.submitLoginForm();
   await moviesPage.checkLoggedIn();
+
+  const movie = moviesData[0];
+
+  await moviesPage.create(
+    movie.title,
+    movie.description,
+    movie.company,
+    movie.releaseYear
+  );
+
+  await page.click('button:has-text("Cadastrar")');
 });
 
---
+++ tests/pages/admin/Movies.ts
@@ -0,0 +1,43 @@
+import { expect, Page } from '@playwright/test';
+
+export class MoviesPage {
+  private page: Page;
+
+  constructor(page) {
+    this.page = page;
+  }
+
+  async checkLoggedIn() {
+    await this.page.waitForLoadState('domcontentloaded');
+    await expect(this.page).toHaveURL(/\/admin\/movies/);
+  }
+
+  async create(
+    title: string,
+    overview: string,
+    company: string,
+    releaseYear: string
+  ) {
+    await this.page.click('a[href*="register"]');
+    await this.page.waitForURL(/register/i);
+
+    await this.page.fill('#title', title);
+    await this.page.fill('#overview', overview);
+
+    await this.page.locator('#select_company_id').click();
+    await this.page
+      .locator('.react-select__option')
+      .filter({
+        hasText: company,
+      })
+      .click();
+
+    await this.page.locator('#select_year').click();
+    await this.page
+      .locator('.react-select__option')
+      .filter({
+        hasText: releaseYear,
+      })
+      .click();
+  }
+}
