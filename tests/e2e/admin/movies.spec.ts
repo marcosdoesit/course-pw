419e4d0e8140895688750296a82b50651bf4cfec
Marcos Souza <marcosfromrio@protonmail.com>
2024-10-31 17:06:20 -0300

    chore: simplify admin movie tests by centralizing page object usage and updating imports

--- tests/e2e/admin/movies.spec.ts
+++ tests/e2e/admin/movies.spec.ts
@@ -1,34 +1,17 @@
-import { test } from '@playwright/test';
-
-import { LoginAdminPage } from '@pages/admin';
-import { MoviesPage } from '@pages/admin/Movies';
-
-import { ToastComponent } from '@components/Toast';
-
-import { Movie } from '@support/types';
+import { test } from '@support/index';
 import { runQuery } from '@support/db';
 
-const moviesData: Movie = require('@support/fixtures/movies.json');
-
-let loginPage: LoginAdminPage,
-  toast: ToastComponent,
-  moviesPage: MoviesPage;
-
-test.beforeEach(async ({ page }) => {
-  loginPage = new LoginAdminPage(page);
-  toast = new ToastComponent(page);
-  moviesPage = new MoviesPage(page);
-});
+import moviesData from '@support/fixtures/movies.json';
 
 test('Admin can add a movie', async ({ page }) => {
   const movie = moviesData.guerra_mundial_z;
   await runQuery(`DELETE FROM movies WHERE title = '${movie.title}'`);
 
-  await loginPage.open();
-  await loginPage.submitLoginForm();
-  await moviesPage.checkLoggedIn();
+  await page.login.open();
+  await page.login.submitLoginForm();
+  await page.movies.checkLoggedIn();
 
-  await moviesPage.create(
+  await page.movies.create(
     movie.title,
     movie.overview,
     movie.company,
