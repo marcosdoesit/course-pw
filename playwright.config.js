c00f3517e98a1b2575f6478d7446699f60622844
Marcos Souza <marcosfromrio@protonmail.com>
2024-10-31 11:40:46 -0300

    chore: update pre-push hook and comment out baseURL in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -40,7 +40,7 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    baseURL: process.env.BASE_URL,
+    // baseURL: process.env.BASE_URL,
     trace: 'retain-on-failure',
   },
 
